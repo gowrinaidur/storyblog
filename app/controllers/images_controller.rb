91e06e218f0972e31358e6a58936d2a0707bebf6
gowrinaidur <gowrinaidur@gmail.com>
2014-07-20 03:35:57 +0530

    solved issue

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,5 @@
 class ImagesController < ApplicationController
   def index
-  	@stories=Story.includes(:images).order("rand()")#.paginate(:page => params[:page])
+  	@stories=Story.includes(:images).order("random()")#.paginate(:page => params[:page])
   end
 end
