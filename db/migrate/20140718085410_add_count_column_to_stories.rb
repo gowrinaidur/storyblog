8dc4f3473ee8ec869f66691ea46384686f0ec637
gowrinaidur <gowrinaidur@gmail.com>
2014-07-20 02:25:34 +0530

    Code pushed into heroku

--- db/migrate/20140718085410_add_count_column_to_stories.rb
+++ db/migrate/20140718085410_add_count_column_to_stories.rb
@@ -1,6 +1,6 @@
 class AddCountColumnToStories < ActiveRecord::Migration
   def change
-  	remove_column :stories ,:count  
+  	#remove_column :stories ,:count  
   	#add_column :stories, :count, :integer
   end
 end
