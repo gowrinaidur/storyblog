e47eb35e818601fc6ae70506870f23cd425dab26
gowrinaidur <gowrinaidur@gmail.com>
2014-07-26 21:12:33 +0530

    solved issues

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,6 @@
 class ImagesController < ApplicationController
   def index
-  	@stories=Story.includes(:images).order("random()")#.paginate(:page => params[:page])
+  	@stories=Story.includes(:images).order("rand()")#.mysql
+  	#@stories=Story.includes(:images).order("random()")#.postgresql
   end
 end
